c2034f7aff3ca828e92a35a6f2267c22050995a9
Ronak Tandon <111975349+RonakT-mob@users.noreply.github.com>
2023-03-05 21:51:12 +0530

    RonakT - 05/03 - Component binded

--- components/header.js
+++ components/header.js
@@ -20,24 +20,12 @@ class Header extends HTMLElement {
           <li><a href="#services">Services</a></li>
           <li><a href="#portfolio">Projects</a></li>
           <li><a href="#team">CEO</a></li>
-          <!-- <li><a href="blog.html">Blog</a></li>
-          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+          <li class="dropdown"><a href="#"><span>Schemes</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
             <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
+              <li><a href="Projects/index.html">Aanira - I</a></li>
+              <li><a href="#">Aanira Polycast</a></li>
             </ul>
-          </li> -->
+          </li>
           <li><a href="#contact">Contact</a></li>
         </ul>
       </nav><!-- .navbar -->
